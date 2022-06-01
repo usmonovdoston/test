e1f2d25b318a94e54fecbb976f41de4ad5804037
usmonovdoston <73227322+usmonovdoston@users.noreply.github.com>
2022-06-01 08:53:37 +0500

    Add files via upload

---
+++ AutoloadExample.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace doston\test;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+
+        return "Hello!";
+    }
+}
